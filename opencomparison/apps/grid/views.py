40321eef07a65bf880c652285254f6c289d90fc3
Ben <ben.racine@cornerstonenw.com>
2011-11-18 21:28:23 -0800

    something

--- opencomparison/apps/grid/views.py
+++ opencomparison/apps/grid/views.py
@@ -379,16 +379,16 @@ def delete_grid(request, slug):
     return HttpResponseRedirect('/grids/')
 
 
-#@login_required
-def delete_feature(request, id):
-    """
+@login_required
+def delete_feature(request, feature_title, grid_slug):
     """
 
     if not request.user.get_profile().can_edit_grid_feature:
         return HttpResponseForbidden("permission denied")
+    """
 
-    feature_to_delete = Feature.objects.filter(pk=id)
+    feature_to_delete = Feature.objects.filter(title=feature_title)
     feature_to_delete.delete()
 
-    return HttpResponseRedirect('/grids/')
-        
+    redirect = '/grids/g/' + grid_slug
+    return HttpResponseRedirect(redirect)
