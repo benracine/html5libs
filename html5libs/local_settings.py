16dd95f291b607701a8d2aba7634ca3a2d762d71
Ben <ben.racine@cornerstonenw.com>
2011-11-22 18:22:33 -0800

    Reenabling the pypi field in the package form.

--- html5libs/local_settings.py
+++ html5libs/local_settings.py
@@ -3,7 +3,7 @@ import logging
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2", # Add "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
-        "NAME": "test4",          # Or path to database file if using sqlite3.
+        "NAME": "dev",          # Or path to database file if using sqlite3.
         "USER": "postgres",              # Not used with sqlite3.
         "PASSWORD": "",                  # Not used with sqlite3.
         "HOST": "localhost",             # Set to empty string for localhost. Not used with sqlite3.
