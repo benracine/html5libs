3ae48239f82175b640c874960a1260ce7f5acde3
Ben <ben.racine@cornerstonenw.com>
2011-11-22 16:01:14 -0800

    many devops mods

--- add.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -am 'something'
-git push heroku master
---
+++ opencomparison/fabfile.py
@@ -0,0 +1,13 @@
+from fabric.api import local
+
+def test():
+    local("./manage.py test")
+
+def commit():
+    local("git add . && git commit")
+
+def deploy(prudent=True):
+    if prudent:
+        test()
+        commit()
+    local("git push heroku master")
