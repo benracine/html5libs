b967f15e71ebaf17f1ce23af495342760695f9c2
Ben <ben.racine@cornerstonenw.com>
2011-11-22 16:49:08 -0800

    Added ability to add a superuser.

--- html5libs/fabfile.py
+++ html5libs/fabfile.py
@@ -13,3 +13,11 @@ def deploy(prudent=True):
     """
     commit()
     local("git push heroku master")
+
+def deployment_package_updater():
+    local("heroku run html5libs/manage.py package_updater")
+
+def deployment_superuser_add(username, email):
+    command = "heroku run html5libs/manage.py createsuperuser --username %s --email %s" %(username, email)
+    local(command)
+
