c7d90cbb5781f44636f3bdcc1c5e846e2370f24c
Ben <ben.racine@cornerstonenw.com>
2011-11-30 00:05:17 -0800

    deploy

--- html5libs/local_settings.py
+++ html5libs/local_settings.py
@@ -14,7 +14,7 @@ DATABASES = {
 FRAMEWORK_TITLE = "HTML5"
 SITE_TITLE = "Modern Client-Side Libraries"
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 SERVE_MEDIA = False
 #TEST_RUNNER = 'testrunner.OurTestRunner'
